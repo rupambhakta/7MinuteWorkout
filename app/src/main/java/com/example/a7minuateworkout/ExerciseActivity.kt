4866d5958e89446c74a856cb0b40f5d2ce38c927
Rupam <rupambhakta2020@gmail.com>
2023-12-29 14:28:04 +0530

    sdfjyh

--- app/src/main/java/com/example/a7minuateworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuateworkout/ExerciseActivity.kt
@@ -2,10 +2,14 @@ package com.example.a7minuateworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minuateworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding : ActivityExerciseBinding? = null
+    private var restTimer:CountDownTimer? = null
+    private var restProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -19,5 +23,37 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolBarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+        setupRestView()
+    }
+    private fun setupRestView(){
+        if (restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10-restProgress
+                binding?.tvTimer?.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Here we will start the exercise",Toast.LENGTH_SHORT).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
