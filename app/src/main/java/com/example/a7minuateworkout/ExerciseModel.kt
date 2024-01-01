bb1a2bea75d2b2419e16bcf6e2f182d95618e3ef
Rupam <rupambhakta2020@gmail.com>
2024-01-01 14:22:54 +0530

    a

--- app/src/main/java/com/example/a7minuateworkout/ExerciseModel.kt
+++ app/src/main/java/com/example/a7minuateworkout/ExerciseModel.kt
@@ -1,4 +1,30 @@
 package com.example.a7minuateworkout
 
-class ExerciseModel {
+class ExerciseModel (
+    private var id:Int,
+    private var name:String,
+    private var image:Int,
+    private var isCompleted:Boolean,
+    private var isSelected:Boolean
+    )
+{
+    fun getId():Int{
+        return  id;
+    }
+    fun setId(id:Int){
+        this.id=id
+    }
+    fun getName():String{
+        return name
+    }
+    fun setName(name:String){
+        this.name = name
+    }
+    fun getImage():Int{
+        return image
+    }
+    fun setImage(Image:Int){
+        this.image = image
+    }
+
 }
